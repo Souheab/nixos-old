fe253552340341ba55d4031fa6995ebe3fe3ee12
Souheab <souheab@protonmail.com>
2024-08-29 16:35:53 -0400

    Add NUR to flake

--- flake.nix
+++ flake.nix
@@ -6,9 +6,10 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @inputs:
+  outputs = { self, nixpkgs, home-manager, nur, ... } @inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {inherit system; config.allowUnfree = true;};
@@ -29,6 +30,7 @@
                 home-manager.useUserPackages = true;
                 home-manager.users.suller = import ./nixo/home.nix;
               }
+              nur.nixosModules.nur
             ];
           };
         };
