82c85866dcd65724fafadd481fb8669a94fd42c7
Souheab <souheab@protonmail.com>
2024-08-31 10:06:46 -0400

    Imrpove nixvim config

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -4,10 +4,49 @@
 
   programs.nixvim = {
     enable = true;
+    opts = {
+      backup = false;
+      clipboard = "unnamedplus";
+      cmdheight = 1;
+      conceallevel = 0;
+      fileencoding = "utf-8";
+      hlsearch = true;
+      ignorecase = true;
+      showtabline = 2;
+      smartcase = true;
+      smartindent = true;
+      splitbelow = true;
+      splitright = true;
+      termguicolors = true;
+      timeoutlen = 400;
+      undofile = true;
+      updatetime = 300;
+      expandtab = true;
+      number = true;
+      relativenumber = true;
+      wrap = false;
+      scrolloff = 8;
+      sidescrolloff = 8;
+    }
     plugins = {
       lualine = {
         enable = true;
       };
+      bufferline = {
+        enable = true;
+      };
+      nvim-tree = {
+        enable = true;
+      };
+      telescope = {
+        enable = true;
+      };
+      treesitter = {
+        enable = true;
+      };
+      which-key = {
+        enable = true;
+      }
     };
   };
 }
