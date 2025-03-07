0e921c313a1aac3fc67ee178241e59d5a3e3b5d5
Souheab <souheab@protonmail.com>
2025-03-07 14:59:52 -0500

    Add picom

--- modules/home/dwm.nix
+++ modules/home/dwm.nix
@@ -1,11 +1,16 @@
 {pkgs, ...}:
 {
+  imports = [
+    ./picom.nix
+  ];
+
   home.file.".config/dwm/startup.sh" = {
     executable = true;
     text = ''
     #!/bin/sh
-    ${pkgs.feh}/bin/feh --bg-scale ~/.config/dwm/wp.jpg
-    ${pkgs.eww}/bin/eww open bar --screen 0
+    ${pkgs.feh}/bin/feh --bg-scale ~/.config/dwm/wp.jpg &
+    ${pkgs.eww}/bin/eww open bar --screen 0 &
+    ${pkgs.picom}/bin/picom &
     '';
   };
 }
